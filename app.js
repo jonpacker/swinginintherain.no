6f4ec8fba90c9a421080cbd5adc421158a306930
Jon Packer <contact@jonpacker.com>
2014-06-07 15:49:43 +0200

    backgrounds in place

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var http = require('http');
 
-var server = http.createServer();
-var app = express(server);
+var app = express();
+var server = http.createServer(app);
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 server.listen(4000);
